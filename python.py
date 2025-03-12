148c3b6134f852cdf1ce818522f11f48b320eeac
Fazail222 <fazailnadeem223@gmail.com>
2025-03-12 20:09:01 +0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,76 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x = 5
+#Initial value of x
+if x>0:
+    print("Two comments")#Print the string
+
+    
+Two comments
+print("Statement1")
+Statement1
+>>> print("Statement1");print("Statement2")
+Statement1
+Statement2
+>>> a=344
+>>> type(a)
+<class 'int'>
+>>> a=3.33
+>>> type(a)
+<class 'float'>
+>>> b=-11.22
+>>> type(b)
+<class 'float'>
+>>> x=complex(3,5)
+>>> type(x)
+<class 'complex'>
+>>> print(x)
+(3+5j)
+>>> b=4+6j
+>>> type(b)
+<class 'complex'>
+>>> print(b)
+(4+6j)
+>>> x=True
+>>> type(x)
+<class 'bool'>
+>>> string1="String"
+>>> print(string1)
+String
+>>> str2='string"a'
+>>> print(str2)# Double quote within single quotes
+string"a
+>>> print("This is backslash (\\) mark")
+This is backslash (\) mark
+>>> print("This is tab \t mark")
+This is tab 	 mark
+>>> string1 = "I Love pakistan"
+>>> print(string1[0])
+I
+>>> print(string1[-15])
+I
+>>> print(string1[5])
+e
+listmy=[4,6,23,5,66,4]
+print(listmy)
+[4, 6, 23, 5, 66, 4]
+listmy=["Red","Green","Blue","black","White"]
+print(listmy[1])
+Green
+print(listmy[-5])
+Red
+print(listmy)
+['Red', 'Green', 'Blue', 'black', 'White']
+listmy[0]
+'Red'
+color=["Red","Green","Blue","black","White"]
+print(color[0:2]) #cut first two items
+['Red', 'Green']
+print(color[1:2]) #cut second item
+['Green']
+print(color[1:-2]) # cut second item
+['Green', 'Blue']
+print(color[:3]) # cut first three items
+['Red', 'Green', 'Blue']
+print(color[:])# creates copy of original list
+['Red', 'Green', 'Blue', 'black', 'White']

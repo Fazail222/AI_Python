280373e4123c7b7f13ec1e0e8006e0a693691d5c
Fazail222 <fazailnadeem223@gmail.com>
2025-04-10 11:46:08 +0500

    Update python3.py

--- python3.py
+++ python3.py
@@ -1,94 +1,110 @@
-import random
+# import random
 
-# Temperature Converter
-def conv_temp(val, u):
-    res = 0
-    if u == 'c':
-        res = (val * 9 / 5) + 32
-        print(str(res) + 'F')
-    elif u == 'f':
-        res = (val - 32) * 5 / 9
-        print(str(res) + 'C')
+# #1 Temperature Converter
+# def temp(val, a):
+#     result = 0
+#     if a == 'c':
+#         result = (val * 9 / 5) + 32
+#         print(str(res) + 'F')
+#     elif a== 'f':
+#         result = (val - 32) * 5 / 9
+#         print(str(res) + 'C')
 
-conv_temp(60, 'c')
-conv_temp(45, 'f')
+# temp(60, 'c')
+# temp(45, 'f')
 
-# Find numbers divisible by both 5 and 7
-for n in range(1500, 2701):
-    if n % 7 == 0 and n % 5 == 0:
-        print(n)
+# #2 Find numbers divisible by both 5 and 7
+# for n in range(1500, 2701):
+#     if n % 7 == 0 and n % 5 == 0:
+#         print(n)
 
-# Number Guessing Game
-num = random.randint(1, 9)
-while True:
-    g = int(input("Guess a number between 1 and 9: "))
-    if g == num:
-        print("Well guessed!")
-        break
-    else:
-        print("Wrong!")
+# #3 Number Guess
+# num = random.randint(1, 9)
+# while True:
+#     a = int(input("Guess a number between 1 and 9: "))
+#     if a == num:
+#         print("Well guessed!")
+#         break
+#     else:
+#         print("You are Wrong!")
 
-# Pattern printing
-n = 5
-s = 1
-maxed = False
-for r in range(0, 2 * n):
-    for _ in range(0, s):
-        print("*", end="")
-    print("")
-    if s < n and not maxed:
-        s += 1
-    elif s == n and not maxed:
-        maxed = True
-        s -= 1
-    else:
-        s -= 1
+# #4 Pattern printing
+# n = 5
+# s = 1
+# flag= False
+# for r in range(0, 2 * n):
+#     for _ in range(0, s):
+#         print("*", end="")
+#     print("")
+#     if s < n and not flag:
+#         s += 1
+#     elif s == n and not flag:
+#         flag = True
+#         s -= 1
+#     else:
+#         s -= 1
 
-# Reverse string (slice method)
-w = input("Enter a word: ")
-print(w[::-1])
+# #5 Reverse string
+# w = input("Enter a word: ")
+# print(w[::-1])
 
-# Reverse string (loop method)
-w = input("Enter a word: ")
-rev = ""
-for i in range(1, len(w) + 1):
-    rev += w[-i]
-print(rev)
+# #6 Count even and odd
+# lst = [1, 3, 12, 14, 35, 24]
+# even = 0
+# odd = 0
+# for x in lst:
+#     if x % 2 == 0:
+#         even += 1
+#     else:
+#         odd += 1
+# print("Even:", even, "Odd:", odd)
 
-# Count even and odd
-lst = [1, 3, 12, 14, 35, 24]
-even = 0
-odd = 0
-for x in lst:
-    if x % 2 == 0:
-        even += 1
-    else:
-        odd += 1
-print("Even:", even, "Odd:", odd)
+# #7 Show type of each item
+# data = [6387, 5.55, 4+6j, True, "Hello there", (5, 5), [6, 7], {"a": 6, "b": 5}]
+# for x in data:
+#     print(x, type(x))
 
-# Show type of each item
-data = [123, 1.24, 1+2j, True, "Hi", (2, 5), [2, 5], {"a": 1, "b": 2}]
-for x in data:
-    print(x, type(x))
+# #8 Skip certain numbers
+# for x in range(7):
+#     if x == 3 or x == 6:
+#         continue
+#     print(x, end=" ")
 
-# Skip certain numbers
-for x in range(7):
-    if x == 3 or x == 6:
-        continue
-    print(x, end=" ")
+# #9 Fibonacci until >50
+# a = 0
+# b = 1
+# while True:
+#     print(a)
+#     a = b
+#     b=a + b
+#     if b > 50:
+#         break
 
-# Fibonacci until >50
-a = 0
-b = 1
-while True:
-    print(a)
-    a, b = b, a + b
-    if b > 50:
-        break
+# #10 2D list value assign
+# i = 4
+# j = 3
+# mat = [[0]*5 for _ in range(5)]
+# mat[i][j] = 99
+# print(mat)
 
-# 2D list value assign
-i = 4
-j = 3
-mat = [[0]*5 for _ in range(5)]
-mat[i][j] = 99
-print(mat)
+#11 
+# lines = []
+# while True:
+#     line = input("Enter a line (blank to stop): ")
+#     if not line:
+#         break
+#     lines.append(line.lower())
+
+# for l in lines:
+#     print(l)
+
+
+#12
+data = input("Enter binary numbers separated by commas: ")
+binaries = data.split(',')
+for b in binaries:
+    decimal = int(b, 2)
+    if decimal % 5 == 0:
+        print(b)
+
+
